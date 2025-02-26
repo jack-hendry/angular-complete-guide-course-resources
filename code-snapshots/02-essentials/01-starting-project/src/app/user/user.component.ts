f25c43d4584672ab7815ee309ad2360b4aa07b91
jack-hendry <jack.r.hendry.p@gmail.com>
2025-02-26 13:57:47 -0500

    32. Using Signal Inputs ( deleted a lot of what was done)

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,21 +1,18 @@
-import { Component, Input} from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
-  @Input({required: true}) avatar!: string; //will definetly be set 
-  @Input({required: true}) name!: string; //will definetly be set 
+  @Input({ required: true }) avatar!: string; //will definetly be set
+  @Input({ required: true }) name!: string; //will definetly be set
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
-  onSelectUser() {
-
-  }
+  onSelectUser() {}
 }
