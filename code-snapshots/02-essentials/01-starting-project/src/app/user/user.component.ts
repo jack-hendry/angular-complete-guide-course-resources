ab943289c32e2ea7d467900e6eae9c6d180ae060
jack-hendry <jack.r.hendry.p@gmail.com>
2025-02-27 10:44:05 -0500

    56. Content Projection with ng-content

--- code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -5,12 +5,13 @@ import {
   Output,
 } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
