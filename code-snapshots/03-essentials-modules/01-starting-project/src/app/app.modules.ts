85bab85c6a6d3615c6ce2b7e2a737150b4674ba5
jack-hendry <jack.r.hendry.p@gmail.com>
2025-02-27 14:55:25 -0500

    68. Declaring & Using Components

--- code-snapshots/03-essentials-modules/01-starting-project/src/app/app.modules.ts
+++ code-snapshots/03-essentials-modules/01-starting-project/src/app/app.modules.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-
-
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
-    declarations: [AppComponent,
-        
-    ],
-    bootstrap: [AppComponent], // root component
-    imports: []
+  declarations: [AppComponent], // can not be used for standalone components
+  bootstrap: [AppComponent], // root component
+  imports: [HeaderComponent, TasksComponent, UserComponent, BrowserModule], // can be used for standalone components
 })
-export class AppModule{}
+export class AppModule {}
