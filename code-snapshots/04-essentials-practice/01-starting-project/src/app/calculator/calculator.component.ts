f6ec106709ded652193c60e5fd1fdf4d1c8222bf
jack-hendry <jack.r.hendry.p@gmail.com>
2025-02-28 10:48:42 -0500

    76. Adding a User Input Component

---
+++ code-snapshots/04-essentials-practice/01-starting-project/src/app/calculator/calculator.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css',
+})
+export class CalculatorComponent {
+  initialInvestment: number = 0;
+  anualInvestment: number = 0;
+  expectedReturn: number = 5;
+  duration: number = 10;
+
+  onClick() {
+    console.log(`
+      initialInvestment: ${this.initialInvestment}
+anualInvestment: ${this.anualInvestment}
+expectedReturn: ${this.expectedReturn}
+duration: ${this.duration}
+
+      `);
+  }
+
+  submit(){
+    console.log("submitted")
+  }
+}
