e663a71ae2f500fe412e708ec834382a3b8b3d0f
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-05 16:08:46 -0500

    102. Using Content Projection & ng-content

--- code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,19 +1,13 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
   standalone: true,
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
 })
 export class DashboardItemComponent {
-  // @Input({required: true}) img: { src: string; alt: string } | undefined;
-  // @Input({required: true}) title: string | undefined;
-  // @Input({required: true}) description: string | undefined;
-
-
   img = input.required<{ src: string; alt: string }>();
   title = input.required<string>();
-  // description = input.required<string>();
 }
