8a6727f9579e405c974d178e5dc34e4000c0d675
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-05 15:52:09 -0500

    100. Component Inputs: Repetition

--- code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-item',
@@ -8,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard-item.component.css'
 })
 export class DashboardItemComponent {
+  // @Input({required: true}) img: { src: string; alt: string } | undefined;
+  // @Input({required: true}) title: string | undefined;
+  // @Input({required: true}) description: string | undefined;
 
+
+  img = input.required<{ src: string; alt: string }>();
+  title = input.required<string>();
+  description = input.required<string>();
 }
