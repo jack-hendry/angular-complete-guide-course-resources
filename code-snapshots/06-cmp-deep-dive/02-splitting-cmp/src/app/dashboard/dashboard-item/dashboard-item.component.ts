a3083012a07eb5fffe841d6acbe00988ccaf79b1
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-11 14:13:08 -0400

    115. When (Not) To Rely On Host Elements

--- code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -6,6 +6,10 @@ import { Component, input } from '@angular/core';
   imports: [],
   templateUrl: './dashboard-item.component.html',
   styleUrl: './dashboard-item.component.css',
+  // host: {
+  //   class: 'dashboard-item',
+  // }
+  
 })
 export class DashboardItemComponent {
   img = input.required<{ src: string; alt: string }>();
