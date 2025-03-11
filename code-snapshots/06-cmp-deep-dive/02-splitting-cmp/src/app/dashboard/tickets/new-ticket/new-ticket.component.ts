875b871577ad9ddc1dbab683537a73984e3e9114
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-11 16:02:09 -0400

    132. Using The viewChild Signal Function

--- code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild, } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
@@ -11,10 +11,11 @@ import { ButtonComponent } from '../../../shared/button/button.component';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent  {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText);
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
