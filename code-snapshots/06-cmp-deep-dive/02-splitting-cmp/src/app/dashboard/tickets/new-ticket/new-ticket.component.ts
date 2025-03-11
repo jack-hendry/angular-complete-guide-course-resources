a593badf105489c6ebb173ef56a90f59745270ac
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-11 15:45:03 -0400

    128. Working with Template Variables

--- code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ code-snapshots/06-cmp-deep-dive/02-splitting-cmp/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, FormsModule,ButtonComponent],
+  imports: [ControlComponent, FormsModule, ButtonComponent],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-
-  onSubmit() {
-    console.log('New ticket submitted');
+  onSubmit(titleElement: HTMLInputElement) {
+    const enteredTitle = titleElement.value;
+    console.dir(titleElement.value);
   }
 }
