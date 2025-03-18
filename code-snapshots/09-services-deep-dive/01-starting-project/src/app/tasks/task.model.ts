773e59e1b3a3fd20d1785d9875bd8b47a30e994b
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-18 15:35:43 -0400

    188. Injecting Other Values (NOT Services)

--- code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/task.model.ts
+++ code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/task.model.ts
@@ -1,5 +1,16 @@
+import { InjectionToken, Provider } from '@angular/core';
+
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+type TaskStatusOptions = {
+  value: 'open' | 'in-progress' | 'done';
+  taskStatus: TaskStatus;
+  text: string;
+}[];
+
+export const TASK_STATUS_OPTIONS = new InjectionToken<TaskStatusOptions>(
+  'task-status-options'
+);
 export const TaskStatuesOptions: {
   value: 'open' | 'in-progress' | 'done';
   taskStatus: TaskStatus;
@@ -21,6 +32,11 @@ export const TaskStatuesOptions: {
     text: 'Completed',
   },
 ];
+
+export const taskStatusOptionsProvider: Provider = {
+  provide: TASK_STATUS_OPTIONS,
+  useValue: TaskStatuesOptions,
+};
 
 export interface Task {
   id: string;
--- code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/tasks-list/tasks-list.component.ts
+++ code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../task.service';
 import { TasksServiceToken } from '../../../main';
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -9,10 +10,12 @@ import { TasksServiceToken } from '../../../main';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionsProvider]
 })
 export class TasksListComponent {
   private tasksService = inject(TasksServiceToken);
   private selectedFilter = signal<string>('all');
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
   tasks = computed(() => {
     switch (this.selectedFilter()) {
       case 'open':
