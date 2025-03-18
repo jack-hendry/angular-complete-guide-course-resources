fa6ff508590aa6a28e5c5564a9f2a90a74217cd3
jack-hendry <jack.r.hendry.p@gmail.com>
2025-03-18 15:06:02 -0400

    183. Understanding the Element Injector's Behavior

--- code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/09-services-deep-dive/01-starting-project/src/app/tasks/tasks.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
-import { TasksService } from './task.service';
+// import { TasksService } from './task.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
-  providers: [TasksService],
+  // providers: [TasksService],
 })
 export class TasksComponent {}
